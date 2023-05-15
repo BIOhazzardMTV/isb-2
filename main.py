7d386eae0288f28f606d79525342df58e9fbf4be
BIOhazzardMTV <fedorov200375@mail.ru>
2023-05-16 02:44:09 +0400

    Created a PRNG in C++, split the sequence into cells of 8 bits each, created lists that contain a sequence cast to int type, implemented a frequency bitwise test.

---
+++ main.py
@@ -0,0 +1,34 @@
+import math
+with open("GPSCH.txt", mode='r', encoding='UTF-8') as text:
+    reader = text.read()
+    dic = {}
+    lst = []
+    lst2 = []
+    z = 0
+    x = 0
+    count = 0
+    for i in reader:
+        dic[z] = reader[count:count + 8]
+        lst.append(i)
+        lst2.append(i)
+        x += 1
+        if x == 8:
+            z += 1
+            count += 8
+            x = 0
+    print(dic)
+    print(lst)
+    lst = list(map(int, lst))
+    lst2 = list(map(int, lst))
+    print(lst2)
+
+    # Частотный побитовый тест.
+    for i in range(len(lst)):
+        if lst[i] == 0:
+            lst[i] = -1
+    print(lst)
+    print(sum(lst))
+    S1 = (-1/math.sqrt(128))*sum(lst)
+    print(S1)  # 1.0606601717798212
+    P1 = math.erfc(S1/math.sqrt(2))
+    print(P1)  # 0.2888443663464849
