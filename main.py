4707fc2f7c9b1c5094328dddfcb6b43b8050cd92
BIOhazzardMTV <fedorov200375@mail.ru>
2023-05-16 02:49:47 +0400

    Implemented a test for identical consecutive bits.

--- main.py
+++ main.py
@@ -32,3 +32,22 @@ with open("GPSCH.txt", mode='r', encoding='UTF-8') as text:
     print(S1)  # 1.0606601717798212
     P1 = math.erfc(S1/math.sqrt(2))
     print(P1)  # 0.2888443663464849
+
+    # Тест на одинаковые подряд идущие биты.
+    S2 = (1 / 128) * sum(lst2)
+    print(S2)
+    if abs(S2 - 0.5) < 2 / math.sqrt(128):
+        print("Good :)")
+    else:
+        print("Bad :(")
+    ri = []
+    for i in range(len(lst2) - 1):
+        if lst2[i] == lst2[i + 1]:
+            ri.append(0)
+        else:
+            ri.append(1)
+    print(ri)
+    Vn = sum(ri)
+    print(Vn)
+    P2 = math.erfc((abs(Vn - 2 * 128 * S2 * (1 - S2))) / (2 * math.sqrt(2 * 128) * S2 * (1 - S2)))
+    print(P2)  # 0.42870928611174086
